b9c3bad49a9c9a414c2cf8bbb680055cd3dcac75
itaysegev1 <itaysegev1234@gmail.com>
2024-11-11 14:55:16 +0200

    enter

--- AIPlayer.java
+++ AIPlayer.java
@@ -9,6 +9,7 @@ public abstract class AIPlayer extends Player {
         registerAIPlayerType("RandomAI", RandomAI.class);
         registerAIPlayerType("GreedyAI", GreedyAI.class);
         // Add more AIPlayer subclasses here as needed
+
     }
     @Override
     public boolean isHuman() {
