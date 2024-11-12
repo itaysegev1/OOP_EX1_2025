6f34d687e7a5c46bcfe58d830f8251f1acb05d7c
sasa0412 <timsitmelosa@gmail.com>
2024-11-12 20:37:29 +0200

    khb

--- BombDisc.java
+++ BombDisc.java
@@ -4,20 +4,32 @@ public class BombDisc implements Disc {
     public BombDisc(Player owner){
         Owner=owner;
     }
-
+    /***
+     * Get the player who owns the Disc.
+     * The player who is the owner of Bomb disc.
+     */
     @Override
     public Player getOwner() {
         return Owner;
     }
+    /**
+     * Set the player who owns the BombDisc.
+     *
+     */
 
     @Override
     public void setOwner(Player player) {
         Owner=player;
     }
-
+    /**
+     * Get the type of the disc.
+     *we used the:
+     *        "💣"
+     *       Bomb Disc
+     */
     @Override
     public String getType() {
-        return "\uD83D\uDCA3";
+        return "💣";
     }
 }
 
