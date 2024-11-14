6fdb6b08dca6bdba4c36fc3cedc66da12f0474b4
sasa0412 <timsitmelosa@gmail.com>
2024-11-14 18:37:06 +0200

    j

--- flip2.java
+++ flip2.java
@@ -5,89 +5,38 @@ public class flip2 {
     // הוספתי את זה |
 
 
+    //זה הוספתי מפה והלאה
 
+    ///function number 3
+    private static List<Position> flipbombneighbors(Position pos, int Boardsize, Disc[][] Board, Player player, List<Position> list) {
 
-//        public static List<Disc> directflips (Position a,int directrow, int directcol, Player player,
-//        int BoardSize, Disc[][] Board){
-//            List<Disc> d = new ArrayList<>();
-//            int r = a.row() + directrow;
-//            int c = a.col() + directcol;
-//            while ((r < BoardSize && c < BoardSize) && (r >= 0 && c >= 0)) {
-//                if (Board[r][c] == null)
-//                    return d;
-//                if (Board[r][c].getOwner() != player) {
-//                    if (Board[r][c].getType().equals("⬤")) {
-//                        d.add(Board[r][c]);
-//                        r += directrow;
-//                        c += directcol;
-//                    } else {
-//                        if (Board[r][c].getType().equals("⭕")) {
-//                            r += directrow;
-//                            c += directcol;
-//                        } else {
-//                            if (Board[r][c].getType().equals("\uD83D\uDCA3") && Board[r][c].getOwner() != player) {
-//                                if (!d.contains(Board[r][c])) {
-//                                    d.add(Board[r][c]);
-//                                    List<Disc> n = new ArrayList<>();
-//                                    n.addAll(allsides(new Position(r, c), BoardSize, Board, player));
-//                                    d.addAll(n);
-//                                    r += directrow;
-//                                    c += directcol;
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//
-//            }
-//            return d;
-//        }
-
-//        public static int flipscounter (List < Disc > d) {
-//            int count = 0;
-//            int i = 0;
-//            while (!d.isEmpty() && i != d.size()) {
-//                count++;
-//                i++;
-//            }
-//            return count;
-//        }
-//זה הוספתי מפה והלאה
-        public static List<Disc> flipbombneighbors (Position pos,int Boardsize, Disc[][] Board, Player player){
-            System.out.println("DEBUGGGG");
-            List<Disc> sides = new ArrayList<>();
-            int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-            for (int i = 0; i < directions.length; i++) {
-                int r = pos.row() + directions[i][0];
-                int c = pos.col() + directions[i][1];
-                if (isvalid(r, c, Boardsize) && Board[r][c] != null) {
-                    if (Board[r][c].getOwner() == player) {
-                        sides.add(Board[r][c]);
-                        System.out.println(player.toString() + "flipped the " + Board[r][c].getType() + " in ( " + r + " , " + c + " )");
+        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+        for (int i = 0; i < directions.length; i++) {
+            int r = pos.row() + directions[i][0];
+            int c = pos.col() + directions[i][1];
+            if (isvalid(r, c, Boardsize) && Board[r][c] != null) {
+                if ((Board[r][c].getOwner() != player) && (!Board[r][c].getType().equals("⭕"))) {
+                    if (!Board[r][c].getType().equals("\uD83D\uDCA3")) {
+                        list.add(new Position(r, c));
                     }
-                }
-
-            }
-            return sides;
-        }
-
-        private static boolean isvalid ( int row, int col, int BoardSize){
-            if ((row < BoardSize && col < BoardSize) && (row >= 0 && col >= 0)) {
-                return true;
-            }
-            return false;
-        }
-        private static List<Disc> finallistflip(Player p, List<Disc> fl,Disc[][] Board){
-            List<Disc> flipped = new ArrayList<>();
-            for (int i = 0; i < fl.size(); i++) {
-                if(fl.get(i).getOwner()!=p){
-                    fl.get(i).setOwner(p);
-                    flipped.add(fl.get(i));
+                } else {
+                    flipbombneighbors(new Position(r, c), Boardsize, Board, player, list);
                 }
             }
-            return flipped;
         }
-    private static boolean needtoflip(Position a, int directrow, int directcol, Player player, int BoardSize, Disc[][]Board) {
+        return list;
+    }
+
+    //help to function 3
+    private static boolean isvalid(int row, int col, int BoardSize) {
+        if ((row < BoardSize && col < BoardSize) && (row >= 0 && col >= 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    //function number 1
+    private static boolean needtoflip(Position a, int directrow, int directcol, Player player, int BoardSize, Disc[][] Board) {
         int flips = 0;
         int r = a.row() + directrow;
         int c = a.col() + directcol;
@@ -110,40 +59,80 @@ public class flip2 {
         return false;
     }
 
-    public static List<Disc> fliipedaftermove(Position a, Disc[][]Board, Player firstplayer, Player secondplayer, boolean IsfirstPlayerTurn,int boardsize) {
-        List<Disc> list = new ArrayList<>();
-        List<Position> bomblist = new ArrayList<>();
+    //function number 2!
+    public static List<Position> flipaftermove(Position a, Disc[][] Board, Player firstplayer, Player secondplayer, boolean IsfirstPlayerTurn, List<Position> list, int boardsize) {
         Player p;
-        if (IsfirstPlayerTurn) {
+        if (IsfirstPlayerTurn)
             p = firstplayer;
-        } else {
+        else
             p = secondplayer;
-        }
         int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
         for (int i = 0; i < 8; i++) {
             if (needtoflip(a, directions[i][0], directions[i][1], p, Board.length, Board)) {
                 int r = a.row() + directions[i][0];
                 int c = a.col() + directions[i][1];
-                if (Board[r][c].getType().equals("\uD83D\uDCA3") && !bomblist.contains(Board[r][c])) {
-                    bomblist.add(new Position(r,c));
+                while (Board[r][c].getOwner() != p) {
+                    if (Board[r][c].getType().equals("\uD83D\uDCA3") && !list.contains(new Position(r, c))) {
+                        list.add(new Position(r, c));
+                        flipbombneighbors(new Position(r, c), boardsize, Board, p, list);
+                    }
+                    r += directions[i][0];
+                    c += directions[i][1];
                 }
-                Board[r][c].setOwner(p);
-                if (!Board[r][c].getType().equals("⭕")) {
-                    System.out.println(p.toString() + "flipped the " + Board[r][c].getType() + " in ( " + r + " , " + c + " )");
-                }
-                list.add(Board[r][c]);
-                r += directions[i][0];
-                c += directions[i][1];
             }
         }
-
-        while (!bomblist.isEmpty()) {
-            Position pos = bomblist.removeLast();
-            list.addAll(flipbombneighbors(pos,boardsize,Board,p));
-        }
-            return list;
-        }
+        return list;
     }
+
+    //function number 4
+    public static List<Disc> mainflip(Position a, Disc[][] Board, Player firstplayer, Player secondplayer, boolean IsfirstPlayerTurn, int boardsize) {
+        List<Position> list = new ArrayList<>();
+        List<Disc> list2 = new ArrayList<>();
+        list.add(a);
+        flipaftermove(a, Board, firstplayer, secondplayer, IsfirstPlayerTurn, list, boardsize);
+        Player p;
+        if (IsfirstPlayerTurn)
+            p = firstplayer;
+        else
+            p = secondplayer;
+        for (int i = 0; i < list.size(); i++) {
+            int r = list.get(i).row();
+            int c = list.get(i).col();
+            if (Board[r][c] != null) {
+                Board[r][c].setOwner(p);
+                list2.add(Board[r][c]);
+            }
+
+        }
+        return list2;
+    }
+
+    //function number 5
+    public static int countflip(Position a, Disc[][] Board, Player firstplayer, Player secondplayer, boolean IsfirstPlayerTurn, int boardsize) {
+        List<Position> list = new ArrayList<>();
+        int flip = 0;
+        list.add(a);
+        flip++;
+        flipaftermove(a, Board, firstplayer, secondplayer, IsfirstPlayerTurn, list, boardsize);
+        Player p;
+        if (IsfirstPlayerTurn)
+            p = firstplayer;
+        else
+            p = secondplayer;
+        for (int i = 0; i < list.size(); i++) {
+            int r = list.get(i).row();
+            int c = list.get(i).col();
+            if (Board[r][c] != null) {
+                Board[r][c].setOwner(p);
+                flip++;
+            }
+
+        }
+        return flip;
+    }
+}
+
+
 
 
 
